18a05e4eeddad79988bb11c61cadef2e6aba5af5
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-08-27 10:16:09 -0300

    Criando Home e Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button"
 
 const Page = () => {
     return (
-        <div>
-            <Button>Clique aqui</Button>
-            <ThemeToggle></ThemeToggle>
+        <div className="w-full max-w-4xl mx-auto">
+            <Header/>
+            <div className="mx-3">
+                ...
+            </div>
+            <Footer/>
         </div>
     );
 }
