5345c21a026a8a939ba34b8305fbcb59fd415e16
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-08-27 14:53:53 -0300

    Criando Sidebar

---
+++ src/components/cart/sidebar.tsx
@@ -0,0 +1,40 @@
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { CarIcon } from "lucide-react";
+import { RocketIcon } from "@radix-ui/react-icons";
+import { Separator } from "../ui/separator";
+
+export const CartSidebar = () => {
+    return (
+        <Sheet>
+            <SheetTrigger>
+                <Button>
+                    <RocketIcon className="mr-2" />
+                    <p>Carrinho</p>
+                </Button>
+            </SheetTrigger>
+            <SheetContent>
+                <SheetHeader>
+                    <SheetTitle>Carrinho</SheetTitle>
+                </SheetHeader>
+
+                <div className="flex flex-col gap-5 my-3">
+                    ...
+                </div>
+
+                <Separator className="my-4" />
+
+                <div className="flex justify-between items-center text-xs">
+                    <div>Subtotal:</div>
+                    <div>...</div>
+                </div>
+
+                <Separator className="my-4" />
+
+                <div className="text-center">
+                    <Button>Finalizar Compra</Button>
+                </div>
+            </SheetContent>
+        </Sheet>
+    );
+}
