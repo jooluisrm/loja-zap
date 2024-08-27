a363da8431f5e705ddda0963cd51286a108210f8
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-08-27 15:20:24 -0300

    Criando uma lista Fake de carregamento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
-import { ThemeToggle } from "@/components/theme-toggle";
-import { Button } from "@/components/ui/button"
+import { TabsSkeleton } from "@/components/products/skeleton";
+import { ProductsTab } from "@/components/products/tab";
+import { Suspense } from "react";
 
 const Page = () => {
     return (
         <div className="w-full max-w-4xl mx-auto">
             <Header />
             <div className="mx-3">
-                ...
+                <Suspense fallback={<TabsSkeleton />}>
+                    <ProductsTab />
+                </Suspense>
             </div>
             <Footer />
         </div>
---
+++ src/components/products/tab.tsx
@@ -0,0 +1,22 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export const ProductsTab = () => {
+    return (
+        <Tabs defaultValue="tab1">
+            <TabsList className="flex">
+                <TabsTrigger value="tab1" className="flex-1">
+                    Tab 1
+                </TabsTrigger>
+                <TabsTrigger value="tab2" className="flex-1">
+                    Tab 2
+                </TabsTrigger>
+            </TabsList>
+            <TabsContent value="tab1" className="mt-6">
+                Conteudo da TAB 1
+            </TabsContent>
+            <TabsContent value="tab2" className="mt-6">
+                Conteudo da TAB 2
+            </TabsContent>
+        </Tabs>
+    );
+}
