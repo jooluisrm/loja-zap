24251dfddc12f353c81c5daa51ae55b44c61f070
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-08-29 09:52:33 -0300

    Criando o Store

--- src/components/products/item.tsx
+++ src/components/products/item.tsx
@@ -4,6 +4,7 @@ import { Product } from "@/types/product"
 import { Button } from "@/components/ui/button"
 import { useToast } from "@/components/ui/use-toast"
 import { ToastAction } from "@/components/ui/toast"
+import { useCartStore } from "@/stores/cart-store"
 
 type Props = {
     item: Product
@@ -11,8 +12,10 @@ type Props = {
 
 export const ProductItem = ({ item }: Props) => {
     const { toast } = useToast();
+    const { upsertCartItem } = useCartStore(state => state)
 
     const handleAddButton = () => {
+        upsertCartItem(item, 1);
         toast({
             title: 'Adicionado ao carrinho!',
             description: item.name,
